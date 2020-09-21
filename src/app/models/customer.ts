cfc7024fb7ac84e04dd7f963fc8bdb2fb57365d6
catalansGroup7 <500054301@stu.upes.ac.in>
2020-09-21 13:42:03 +0530

    Frontend, userSignUpTable commit: Sanjeet

---
+++ src/app/models/customer.ts
@@ -0,0 +1,12 @@
+export class Customer{
+    customerId : number;
+    customerFirstName : string;
+    customerLastName : string;
+    customerEmail:string;
+    customerdateOfBirth :Date;
+    customerCity:string;
+    customerMobileNumber:string;
+    customerEmploymentType:string;
+    customerPassword : string;
+
+}
